26e25cc3abe27082c4822bfb30506c33aa56620d
Juliana Aparicio <85405536+JulianaAparicio@users.noreply.github.com>
2022-06-25 22:04:36 -0300

    Delete clinicaOdontologica directory

--- clinicaOdontologica/src/main/java/com/example/clinicaOdontologica/service/serviceException/ServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.example.clinicaOdontologica.service.serviceException;
-
-public class ServiceException extends Exception {
-    public ServiceException(String message) {
-        super(message);
-    }
-}
