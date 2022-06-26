746466bc306c2732892cb6dd304faa2448e13b16
JulianaAparicio <juliana_aparicio@outlook.com>
2022-06-26 12:32:52 -0300

    "Terminé pacienteService"

--- clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/service/PacienteService.java
+++ clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/service/PacienteService.java
@@ -1,4 +1,62 @@
 package com.dh.clinicaOdontologica.service;
 
-public class PacienteService {
+import com.dh.clinicaOdontologica.model.Paciente;
+import com.dh.clinicaOdontologica.model.PacienteDTO;
+import com.dh.clinicaOdontologica.repository.IPacienteRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class PacienteService implements IPacienteService{
+
+    @Autowired
+    private IPacienteRepository pacienteRepository;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Override
+    public void crearPaciente(PacienteDTO pacienteDTO) {
+        guardarPaciente(pacienteDTO);
+    }
+
+    @Override
+    public PacienteDTO buscarPaciente(Long id) {
+        Optional<Paciente> paciente = pacienteRepository.findById(id);
+        PacienteDTO pacienteDTO = null;
+        if(paciente.isPresent()){
+            pacienteDTO = mapper.convertValue(paciente,PacienteDTO.class);
+        }
+        return pacienteDTO;
+    }
+
+    private void guardarPaciente(PacienteDTO pacienteDTO){
+        Paciente paciente = mapper.convertValue(pacienteDTO,Paciente.class);
+        pacienteRepository.save(paciente);
+    }
+
+    @Override
+    public void modificarPaciente(PacienteDTO pacienteDTO) {
+        guardarPaciente(pacienteDTO);
+    }
+
+    @Override
+    public void eliminarPaciente(Long id) {
+        pacienteRepository.deleteById(id);
+    }
+
+    @Override
+    public Set<PacienteDTO> listarTodosLosPacientes() {
+        List<Paciente> pacientes = pacienteRepository.findAll();
+        Set<PacienteDTO> pacientesDTO = new HashSet<>();
+
+        for (Paciente paciente : pacientes){
+            pacientesDTO.add(mapper.convertValue(paciente,PacienteDTO.class));
+        }
+        return pacientesDTO;
+    }
 }
