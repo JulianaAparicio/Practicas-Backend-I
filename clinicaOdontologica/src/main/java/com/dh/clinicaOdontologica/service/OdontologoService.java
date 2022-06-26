1ac7478c1054f4cf2b9bc721cfb07e73f7e94f1c
JulianaAparicio <juliana_aparicio@outlook.com>
2022-06-26 20:55:40 -0300

    "Agregué el OdontologoService"

--- clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/service/OdontologoService.java
+++ clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/service/OdontologoService.java
@@ -1,4 +1,63 @@
 package com.dh.clinicaOdontologica.service;
 
-public class OdontologoService {
+import com.dh.clinicaOdontologica.model.Odontologo;
+import com.dh.clinicaOdontologica.model.OdontologoDTO;
+import com.dh.clinicaOdontologica.repository.IOdontologoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class OdontologoService implements IOdontologoService{
+
+    @Autowired
+    private IOdontologoRepository odontologoRepository;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Override
+    public void crearOdontologo(OdontologoDTO odontologoDTO) {
+        guardarOdontologo(odontologoDTO);
+    }
+
+    @Override
+    public OdontologoDTO buscarOdontologo(Long id) {
+        Optional<Odontologo> odontologo = odontologoRepository.findById(id);
+        OdontologoDTO odontologoDTO = null;
+        if(odontologo.isPresent()){
+            odontologoDTO = mapper.convertValue(odontologo,OdontologoDTO.class);
+        }
+        return odontologoDTO;
+    }
+
+    private void guardarOdontologo(OdontologoDTO odontologoDTO){
+        Odontologo odontologo = mapper.convertValue(odontologoDTO, Odontologo.class);
+        odontologoRepository.save(odontologo);
+    }
+
+    @Override
+    public void modificarOdontologo(OdontologoDTO odontologoDTO) {
+        guardarOdontologo(odontologoDTO);
+    }
+
+    @Override
+    public void eliminarOdontologo(Long id) {
+        odontologoRepository.deleteById(id);
+    }
+
+    @Override
+    public Set<OdontologoDTO> listarTodosLosOdontologos() {
+        List<Odontologo> odontologos = odontologoRepository.findAll();
+        Set<OdontologoDTO> odontologosDTO = new HashSet<>();
+
+        for (Odontologo odontologo : odontologos){
+            odontologosDTO.add(mapper.convertValue(odontologo,OdontologoDTO.class));
+        }
+        return odontologosDTO;    }
 }
--- clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/service/PacienteService.java
+++ clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/service/PacienteService.java
@@ -5,12 +5,14 @@ import com.dh.clinicaOdontologica.model.PacienteDTO;
 import com.dh.clinicaOdontologica.repository.IPacienteRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@Service
 public class PacienteService implements IPacienteService{
 
     @Autowired
