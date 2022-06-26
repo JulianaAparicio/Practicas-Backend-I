600ab63b9a6145240a73beab902c36356017c014
JulianaAparicio <juliana_aparicio@outlook.com>
2022-06-25 22:48:36 -0300

    "Agregue TurnoDTO"

--- clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/model/TurnoDTO.java
+++ clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/model/TurnoDTO.java
@@ -1,5 +1,47 @@
 package com.dh.clinicaOdontologica.model;
 
-public class TurnoDTO {
+import java.time.LocalDate;
 
+public class TurnoDTO {
+    private Long id;
+    private Paciente paciente;
+    private Odontologo odontologo;
+    private LocalDate fecha;
+    private String hora;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Odontologo getOdontologo() {
+        return odontologo;
+    }
+
+    public void setOdontologo(Odontologo odontologo) {
+        this.odontologo = odontologo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
 }
