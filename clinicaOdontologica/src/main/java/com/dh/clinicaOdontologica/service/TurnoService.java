a07a5ce0f67955f6e907e01bc160d3b80e29d2ad
JulianaAparicio <juliana_aparicio@outlook.com>
2022-06-27 18:18:29 -0300

    "Agregué TurnoService"

--- clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/service/TurnoService.java
+++ clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/service/TurnoService.java
@@ -1,4 +1,64 @@
 package com.dh.clinicaOdontologica.service;
 
-public class TurnoService {
+import com.dh.clinicaOdontologica.model.Turno;
+import com.dh.clinicaOdontologica.model.TurnoDTO;
+import com.dh.clinicaOdontologica.repository.ITurnoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class TurnoService implements ITurnoService{
+
+    @Autowired
+    private ITurnoRepository turnoRepository;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Override
+    public void crearTurno(TurnoDTO turnoDTO) {
+        guardarTurno(turnoDTO);
+    }
+
+    @Override
+    public TurnoDTO buscarTurno(Long id) {
+        Optional<Turno> turno = turnoRepository.findById(id);
+        TurnoDTO turnoDTO = null;
+        if(turno.isPresent()){
+            turnoDTO = mapper.convertValue(turno,TurnoDTO.class);
+        }
+        return turnoDTO;
+    }
+
+    private void guardarTurno(TurnoDTO turnoDTO){
+        Turno turno = mapper.convertValue(turnoDTO,Turno.class);
+        turnoRepository.save(turno);
+    }
+
+    @Override
+    public void modificarTurno(TurnoDTO turnoDTO) {
+        guardarTurno(turnoDTO);
+    }
+
+    @Override
+    public void eliminarTurno(Long id) {
+        turnoRepository.deleteById(id);
+    }
+
+    @Override
+    public Set<TurnoDTO> listarTodosLosTurnos() {
+        List<Turno> turnos = turnoRepository.findAll();
+        Set<TurnoDTO> turnosDTO = new HashSet<>();
+
+        for (Turno turno : turnos){
+            turnosDTO.add(mapper.convertValue(turno,TurnoDTO.class));
+        }
+        return turnosDTO;
+    }
 }
