6d9eef084a18d518b45d8568808904345d4f51ad
JulianaAparicio <juliana_aparicio@outlook.com>
2022-07-05 19:09:27 -0300

    "modifique el todosErrores"

--- clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/exceptions/GlobalExceptionHandler.java
+++ clinicaOdontologica/src/main/java/com/dh/clinicaOdontologica/exceptions/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,8 +14,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> todosErrores(Exception ex){
         logger.error(ex.getMessage());
-
-        return new ResponseEntity("Error " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 
     @ExceptionHandler({ResourceNotFoundException.class})
